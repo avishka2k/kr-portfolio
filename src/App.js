9eb2b4607c5d007f7bcca0e81864dca0018dc1f8
romar22 <romardesabille@gmail.com>
2023-10-01 15:48:21 +0800

    added smooth scrolling

--- src/App.js
+++ src/App.js
@@ -7,19 +7,24 @@ import Contacts from "./components/Contacts";
 import Footer from "./components/Footer";
 import Testimonials from "./components/Testimonials";
 import Skills from "./components/Skills";
+import SmoothScroll from "./components/SmoothScroll";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Hero />
-      <Services />
-      <Work />
-      <Experices />
-      <Skills />
-      <Testimonials />
-      <Contacts />
-      <Footer />
+      <SmoothScroll
+        speed={0.09}
+      >
+        <Navbar />
+        <Hero />
+        <Services />
+        <Work />
+        <Experices />
+        <Skills />
+        <Testimonials />
+        <Contacts />
+        <Footer />
+      </SmoothScroll>
     </>
   );
 }
